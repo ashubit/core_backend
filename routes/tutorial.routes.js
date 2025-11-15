53fab64e3e8ca20aef35d0fc1568aca6c638d51b
ashubit <abc@abc.com>
2025-11-15 13:55:17 +0530

    Updated Routes

--- routes/tutorial.routes.js
+++ routes/tutorial.routes.js
@@ -1,36 +1,30 @@
+/**
+ * User Routes Module
+ * Defines authentication and CRUD routes for user management.
+ * @param {Object} app - Express application instance
+ */
 module.exports = app => {
   const user = require("../controllers/user.controller");
   const userValidators = require("../validators/user.validator");
   const auth = require('../middlewares/auth.middleware');
-  
-  const router = require("express").Router();
-  
-  // Create a new Tutorial
-  router.post("/", userValidators.create, user.create);
-  
-  // Retrieve all user
-  router.get("/", userValidators.findAll, user.findAll);
-  
-  // // Retrieve a single Tutorial with id
-  router.get("/:id", userValidators.id, user.findOne);
-  
-  // Update a Tutorial with id
-  router.put("/:id", userValidators.update, user.update);
-  
-  // Delete a Tutorial with id
-  router.delete("/:id", userValidators.id, user.delete);
-  router.get('/me', auth.authenticate, user.profile); 
-  //Get is not working ?
-  // router.post('/me', auth.authenticate, user.profile);
 
-     /**
-     * Login & Signup Routes
-     */
+  const router = require("express").Router();
+
+  /**
+   * Authentication Routes
+   */
   router.post("/login", userValidators.login, user.login);
   router.post("/signup", userValidators.register, user.register);
+  router.get('/me', auth.authenticate, user.profile);
 
-  // Protected route: get current authenticated user's profile
-         
+  /**
+   * User CRUD Routes
+   */
+  router.post("/", userValidators.create, user.create);
+  router.get("/", userValidators.findAll, user.findAll);
+  router.get("/:id", userValidators.id, user.findOne);
+  router.put("/:id", userValidators.update, user.update);
+  router.delete("/:id", userValidators.id, user.delete);
+
   app.use('/api/v1', router);
-    
-  };
+};
